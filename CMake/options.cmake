6d6fdd3a4ae4da9cdfdae5cda654d08f4ae42b5e
Gonzalo Garramuño <ggarra13@gmail.com>
2025-04-05 05:27:02 -0300

    Removed glslang and SPIRV components as they should not be needed with shaderc_combined.

--- CMake/options.cmake
+++ CMake/options.cmake
@@ -675,7 +675,7 @@ endif(FLTK_BUILD_GL)
 
 if(FLTK_BUILD_VK)
     # Required components
-    find_package(Vulkan REQUIRED COMPONENTS glslang shaderc_combined SPIRV-Tools)
+    find_package(Vulkan REQUIRED COMPONENTS shaderc_combined)
 else(FLTK_BUILD_VK)
   set(VK_FOUND FALSE)
   set(HAVE_VK FALSE)
@@ -740,15 +740,7 @@ if(VULKAN_FOUND)
   if (Vulkan_shaderc_combined_FOUND)
       list(APPEND VULKAN_LIBRARIES
 	  Vulkan::shaderc_combined
-	  Vulkan::SPIRV-Tools
-	  Vulkan::glslang
       )
-      if(UNIX AND NOT APPLE)
-	  find_library(SPIRV_Tools_opt_LIBRARY SPIRV-Tools-opt)
-	  if(SPIRV_Tools_opt_LIBRARY)
-	      list(APPEND VULKAN_LIBRARIES ${SPIRV_Tools_opt_LIBRARY})
-	  endif()
-      endif()
   else()
       message(FATAL_ERROR "shaderc_combined not found!")
   endif()
