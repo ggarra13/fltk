9ff0767a248be69b6fa692c9e251c5736ea1f32c
Gonzalo Garramuño <ggarra13@gmail.com>
2025-10-20 09:40:27 -0300

    Minor code improvement.

--- src/drivers/Wayland/Fl_Wayland_Gl_Window_Driver.cxx
+++ src/drivers/Wayland/Fl_Wayland_Gl_Window_Driver.cxx
@@ -407,9 +407,11 @@ void Fl_Wayland_Gl_Window_Driver::resize(int is_a_resize, int W, int H) {
 }
 
 void Fl_Wayland_Gl_Window_Driver::after_resize() {
-  struct wld_window *xid = fl_wl_xid(pWindow);
-  if (pWindow->parent() && xid && !xid->frame_cb)
+  Fl_Window *parent = (pWindow->parent() ? pWindow->window() : NULL);
+  struct wld_window *xid = (parent ? fl_wl_xid(parent) : NULL);
+  if (xid && !xid->frame_cb) {
     eglSwapBuffers(Fl_Wayland_Gl_Window_Driver::egl_display, egl_surface);
+  }
 }
 
 char Fl_Wayland_Gl_Window_Driver::swap_type() {
