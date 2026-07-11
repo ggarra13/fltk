ebd9685221693ace1265bdad0858c4164d4ed207
Gonzalo Garramuño <ggarra13@gmail.com>
2026-07-11 19:59:18 -0300

    Use full qualifier name for Fl::Pen::Driver.

--- src/drivers/WinAPI/Fl_WinAPI_Pen_Events.cxx
+++ src/drivers/WinAPI/Fl_WinAPI_Pen_Events.cxx
@@ -142,11 +142,10 @@ static int g_next_pen_id { 1 };
 namespace Fl {
 namespace Pen {
 
-Driver& newPenDriver() {
+Fl::Pen::Driver& newPenDriver() {
     Fl::Pen::Driver *winapi_driver_instance = new WinAPI_Driver();
     return *winapi_driver_instance;
 }
-
 
 Trait WinAPI_Driver::traits() {
   // The Pointer Input API used by this driver is available on Windows 8 and
