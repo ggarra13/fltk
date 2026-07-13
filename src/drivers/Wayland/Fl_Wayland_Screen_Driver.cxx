a73cf9febadd1754b6b6047be373e726fed50799
Gonzalo Garramuño <ggarra13@gmail.com>
2026-07-13 20:49:09 -0300

    Removed old key checking that used not to work properly on Ubuntu 24.04 LTS.

--- src/drivers/Wayland/Fl_Wayland_Screen_Driver.cxx
+++ src/drivers/Wayland/Fl_Wayland_Screen_Driver.cxx
@@ -881,8 +881,7 @@ static void wl_keyboard_key(void *data, struct wl_keyboard *wl_keyboard,
   Fl_Window *win = ( Fl::focus() ? Fl::focus()->top_window() :
                     Fl_Wayland_Window_Driver::surface_to_window(seat->keyboard_surface) );
   if (event == FL_KEYDOWN && status == XKB_COMPOSE_NOTHING &&
-      !(sym >= FL_Shift_L && sym <= FL_Alt_R) &&
-      !(sym >= FL_F && sym <= FL_F_Last)) {
+      !(sym >= FL_Shift_L && sym <= FL_Alt_R)) {
     // Handling of key repeats :
     // Use serial argument rather than time to detect repeated keys because
     // serial value changes at each key up or down in all tested OS and compositors,
